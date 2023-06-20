01f94d3a3205ca7ad18f50a71fb039de72f21f6f
Lucas <pv.lucassilva@gmail.com>
2023-06-20 14:53:32 -0300

    att produtossql

--- models/produtosModel.sql
+++ models/produtosModel.sql
@@ -1,3 +1,4 @@
+use bancodedados;
 CREATE TABLE produtos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(255) NOT NULL,
@@ -5,3 +6,6 @@ CREATE TABLE produtos (
   preco DECIMAL(10, 2) NOT NULL,
   data_atualizado DATETIME NOT NULL
 );
+
+
+
